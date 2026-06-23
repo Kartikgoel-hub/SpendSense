ae71c97aa6f276808a5cef04fd682988337eb9d1
SRV-KILLER09 <992501030399@mail.jiit.ac.in>
2026-06-23 13:41:38 +0530

    Add advanced search options

--- src/Search.cpp
+++ src/Search.cpp
@@ -1,11 +1,15 @@
 #include "../include/Search.h"
 #include <iostream>
+
 using namespace std;
+
 void Search::searchByID(Expense expense)
 {
     int id;
+
     cout << "Enter Expense ID: ";
     cin >> id;
+
     if(expense.expenseID == id)
     {
         cout << "\nExpense Found!\n";
@@ -16,13 +20,55 @@ void Search::searchByID(Expense expense)
         cout << "\nExpense Not Found!\n";
     }
 }
+
 void Search::searchByCategory(Expense expense)
 {
     string category;
+
     cin.ignore();
+
     cout << "Enter Category: ";
     getline(cin, category);
+
     if(expense.category == category)
+    {
+        cout << "\nExpense Found!\n";
+        expense.displayExpense();
+    }
+    else
+    {
+        cout << "\nExpense Not Found!\n";
+    }
+}
+
+void Search::searchByDate(Expense expense)
+{
+    string date;
+
+    cin.ignore();
+
+    cout << "Enter Date: ";
+    getline(cin, date);
+
+    if(expense.date == date)
+    {
+        cout << "\nExpense Found!\n";
+        expense.displayExpense();
+    }
+    else
+    {
+        cout << "\nExpense Not Found!\n";
+    }
+}
+
+void Search::searchByAmount(Expense expense)
+{
+    double amount;
+
+    cout << "Enter Amount: ";
+    cin >> amount;
+
+    if(expense.amount == amount)
     {
         cout << "\nExpense Found!\n";
         expense.displayExpense();
