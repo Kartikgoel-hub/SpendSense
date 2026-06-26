03cb100bcaa11bfb142df05994497395300e7e92
nishitarastogi40013 <992501040013@mail.jiit.ac.in>
2026-06-26 11:43:25 +0530

    Implement user registration and profile display

--- include/User.h
+++ include/User.h
@@ -1,19 +1,14 @@
 #ifndef USER_H
 #define USER_H
 #include <string>
+
 class User
 {
 public:
-    std::string
-username;
+    std::string username;
     std::string email;
-    std::string
-password;
-    void
-registerUser();
-    void
-displayProfile();
+    std::string password;
 
+void registerUser();
+void displayProfile();
 };
-
-#endif
